1a95702c3902321bd693baa9efee7bec0ca68dde
Bazooka J <grubcowboy@gmail.com>
2024-07-29 21:16:57 -0700

    first commit and initial unit tests

---
+++ queue.js
@@ -0,0 +1,21 @@
+export class Queue {
+    constructor() {
+        this.items = [];
+    }
+
+    push(item) {
+        // Add value to end of queue
+        this.items.push(item);
+    }
+
+    pop() {
+        // Pop head value
+        // return this.items.splice(0, 1)[0];
+        return this.items.shift();
+    }
+
+    peek() {
+        // Remove and return head value
+        return this.items[0];
+    }
+}
---
+++ queue.test.js
@@ -0,0 +1,33 @@
+import { expect, test } from "bun:test";
+import { Queue } from "./queue"
+
+test("push to empty queue", () => {
+
+    const qt = new Queue();
+    expect(qt.peek()).toBe(undefined);
+
+    qt.push(1);
+    expect(qt.peek()).toBe(1);
+});
+
+test("pop from single item queue", () => {
+
+    const qt = new Queue();
+    qt.push(1);
+
+    expect(qt.pop()).toBe(1);
+    expect(qt.peek()).toBe(undefined);
+});
+
+test("pop from empty queue", () => {
+    const qt = new Queue();
+    expect(qt.pop()).toBe(undefined);
+});
+
+test("pop from multiple item queue", () => {
+    const qt = new Queue();
+    qt.push(1);
+    qt.push(2);
+
+    expect(qt.pop()).toBe(1);
+});
